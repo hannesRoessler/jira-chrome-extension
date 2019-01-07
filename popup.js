d415498c05de44a0342855a44c4e37edf781b6d6
hannesRoessler <hannes.roessler@gmx.de>
2019-01-07 20:14:26 +0100

    changes

--- popup.js
+++ popup.js
@@ -1,23 +1,40 @@
 let submitBtn = document.getElementById('submit-btn')
 let issueIdInput = document.getElementById('id-input')
-let projectKeyInput = document.getElementById('project-input')
+let projectKeySelect = document.getElementById('project-select')
 let jiraBaseUrl = "https://miamed.atlassian.net/browse/"
 
 
 function openNewTab() {
-    let url = jiraBaseUrl + projectKeyInput.value + "-" + issueIdInput.value
+    let url = jiraBaseUrl + projectKeySelect.value + "-" + issueIdInput.value
     chrome.tabs.create({'url': url});
 }
 
 
 // Submit on Button click
 submitBtn.onclick = openNewTab;
+projectKeySelect.onchange = saveDropDownSelection;
+window.onload = loadDropDownPreset;
 
 // Submit on Enter key press
 issueIdInput.onkeyup = () => {
     if (event.keyCode === 13) {
         openNewTab();
      }
+}
+
+function saveDropDownSelection() {
+    chrome.storage.local.set({
+        projectKey: projectKeySelect.value
+});
+}
+
+function loadDropDownPreset() {
+    chrome.storage.local.get({
+        projectKey: '',
+    }, function(items) {
+        //Store retrieved options as the selected values in the DOM
+        projectKeySelect.value = items.projectKey;
+    });
 }
 
 
