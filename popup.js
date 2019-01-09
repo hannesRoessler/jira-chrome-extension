ed30767e9f39df71fa23883244e46571d6a16f3e
hannesRoessler <hannes.roessler@gmx.de>
2019-01-09 08:12:18 +0100

    store variables

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
 let submitBtn = document.getElementById('submit-btn')
+let addProjBtn = document.getElementById('addproject-btn')
+let addProjKeyInput = document.getElementById('addProjectKey-input')
+let addProjKeyBtn = document.getElementById('addProjectKey-btn')
+let modal = document.getElementById('modal')
+let modalBackground = document.getElementById('modal-background')
 let issueIdInput = document.getElementById('id-input')
 let projectKeySelect = document.getElementById('project-select')
 let jiraBaseUrl = "https://miamed.atlassian.net/browse/"
+let projects = []
 
 
 function openNewTab() {
@@ -11,9 +17,11 @@ function openNewTab() {
 
 
 // Submit on Button click
+addProjBtn.onclick = openModal;
+addProjKeyBtn.onclick = addProjectKey;
 submitBtn.onclick = openNewTab;
 projectKeySelect.onchange = saveDropDownSelection;
-window.onload = loadDropDownPreset;
+window.onload = useStoredOptionsForDisplayInDOM;
 
 // Submit on Enter key press
 issueIdInput.onkeyup = () => {
@@ -22,20 +30,40 @@ issueIdInput.onkeyup = () => {
      }
 }
 
-function saveDropDownSelection() {
+function saveDropDownSelection(key) {
     chrome.storage.local.set({
         projectKey: projectKeySelect.value
 });
 }
 
-function loadDropDownPreset() {
+function useStoredOptionsForDisplayInDOM() {
     chrome.storage.local.get({
-        projectKey: '',
+        projectKeys: '',
     }, function(items) {
         //Store retrieved options as the selected values in the DOM
-        projectKeySelect.value = items.projectKey;
+        items.projectKeys.map(key => {
+        projectKeySelect.innerHTML += `<option>${key}</option>`;
+    })
     });
+}
+
+function addProjectKey(){
+   projects.push(addProjKeyInput.value)
+   chrome.storage.local.set({
+    projectKeys: projects
+   })
+    closeModal()
+    useStoredOptionsForDisplayInDOM()
+}
+
+
+function openModal(){
+    modal.style.display = "block"
+}
+function closeModal(){
+    modal.style.display = "none"
 }
 
 
 
+//        <option value="NEXT">NEXT</option>
